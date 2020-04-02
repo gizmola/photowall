943b4aca55df91f95a813a4594babfa3c430690e
David Rolston <gizmola@gmail.com>
2020-04-01 23:23:13 -0700

    Lifecycle events

---
+++ src/Components/AddPhoto.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+class AddPhoto extends Component {
+    render() {
+        return <h1>This is where we will add photos</h1>
+    }
+}
+export default AddPhoto
--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Photo from './Photo'
+import PropTypes from 'prop-types'
 
 function Photowall(props) {
-    return <div className="photoGrid">
-        {props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+    return <div>
+        <button className="addIcon"> + </button> 
+        <div className="photoGrid">
+            {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />)}
+        </div>
     </div>
+}
+
+Photowall.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
 }
 
 export default Photowall
