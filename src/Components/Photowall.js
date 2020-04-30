6f4973fadbb96ef3e966d1642411be699f0bac04
David Rolston <gizmola@gmail.com>
2020-04-30 01:44:21 -0700

    Adding photo using setState

--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -6,9 +6,12 @@ import {Link} from 'react-router-dom'
 function Photowall(props) {
     return <div>
         <Link to="/AddPhoto" className="addIcon"> + </Link>
-        {/* <button className="addIcon" onClick={props.onNavigate}> + </button> */}
         <div className="photoGrid">
-            {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />)}
+            {props.posts
+            .sort(function(x,y) {
+                return y.id - x.id
+            })
+            .map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />)}
         </div>
     </div>
 }
